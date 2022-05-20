55d0ed8c09df594669724edb037106d395a5a112
d4rk0s <gregory@d4rk0s.net>
2022-05-20 09:15:36 +0200

    Mise en place des réductions fiscales en fonction du type de customer

--- src/Enums/CustomerType.php
+++ src/Enums/CustomerType.php
@@ -6,4 +6,12 @@ enum CustomerType : string
 {
     case INDIVIDUAL = 'individual';
     case COMPANY = 'company';
+
+    public function getFiscalReduction() : string
+    {
+        return match($this) {
+            self::INDIVIDUAL => 66,
+            self::COMPANY => 60
+        };
+    }
 }
