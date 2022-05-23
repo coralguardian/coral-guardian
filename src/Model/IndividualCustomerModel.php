b3a78f54991e0b10d5284919d9dd07018609568b
d4rk0s <gregory@d4rk0s.net>
2022-05-23 16:27:20 +0200

    test heritage

--- src/Model/IndividualCustomerModel.php
+++ src/Model/IndividualCustomerModel.php
@@ -2,9 +2,7 @@
 
 namespace D4rk0snet\Coralguardian\Model;
 
-use D4rk0snet\Coralguardian\Enums\Language;
-
-class IndividualCustomerModel
+class IndividualCustomerModel extends CustomerModel
 {
     /**
      * @required
@@ -15,116 +13,6 @@ class IndividualCustomerModel
      * @required
      */
     private string $lastname;
-
-    /**
-     * @required
-     */
-    private string $address;
-
-    /**
-     * @required
-     */
-    private string $postalCode;
-
-    /**
-     * @required
-     */
-    private string $city;
-
-    /**
-     * @required
-     */
-    private string $country;
-
-    /**
-     * @required
-     */
-    private string $email;
-
-    /**
-     * @required
-     */
-    private bool $wantsNewsletter;
-
-    /**
-     * @required
-     */
-    private Language $language;
-
-    public function getAddress(): string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-        return $this;
-    }
-
-    public function getCity(): string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): self
-    {
-        $this->city = $city;
-        return $this;
-    }
-
-    public function getCountry(): string
-    {
-        return $this->country;
-    }
-
-    public function setCountry(string $country): self
-    {
-        $this->country = $country;
-        return $this;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-        return $this;
-    }
-
-    public function getPostalCode(): string
-    {
-        return $this->postalCode;
-    }
-
-    public function setPostalCode(string $postalCode): self
-    {
-        $this->postalCode = $postalCode;
-        return $this;
-    }
-
-    public function wantsNewsletter(): bool
-    {
-        return $this->wantsNewsletter;
-    }
-
-    public function getLanguage(): Language
-    {
-        return $this->language;
-    }
-
-    public function setLanguage(string $language): self
-    {
-        try {
-            $this->language = Language::from($language);
-            return $this;
-        } catch (\ValueError $exception) {
-            throw new \Exception("Code de langue invalide",400);
-        }
-    }
 
     public function getFirstname(): string
     {
@@ -145,12 +33,6 @@ class IndividualCustomerModel
     public function setLastname(string $lastname): IndividualCustomerModel
     {
         $this->lastname = $lastname;
-        return $this;
-    }
-
-    public function setWantsNewsletter(bool $wantsNewsletter): IndividualCustomerModel
-    {
-        $this->wantsNewsletter = $wantsNewsletter;
         return $this;
     }
 }
