8cd2ae045e27718ac329d3480e07b3d5089fe870
d4rk0s <gregory@d4rk0s.net>
2022-05-23 15:58:32 +0200

    bugfix

--- src/Model/CompanyCustomerModel.php
+++ src/Model/CompanyCustomerModel.php
@@ -2,7 +2,9 @@
 
 namespace D4rk0snet\Coralguardian\Model;
 
-class CompanyCustomerModel extends AbstractCustomerModel
+use D4rk0snet\Coralguardian\Enums\Language;
+
+class CompanyCustomerModel
 {
     /**
      * @required
@@ -21,6 +23,116 @@ class CompanyCustomerModel extends AbstractCustomerModel
 
     private ?string $alternateNewsletterEmail = null;
 
+    /**
+     * @required
+     */
+    private string $address;
+
+    /**
+     * @required
+     */
+    private string $postalCode;
+
+    /**
+     * @required
+     */
+    private string $city;
+
+    /**
+     * @required
+     */
+    private string $country;
+
+    /**
+     * @required
+     */
+    private string $email;
+
+    /**
+     * @required
+     */
+    private bool $wantsNewsletter;
+
+    /**
+     * @required
+     */
+    private Language $language;
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): AbstractCustomerModel
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): AbstractCustomerModel
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): AbstractCustomerModel
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): AbstractCustomerModel
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPostalCode(): string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(string $postalCode): AbstractCustomerModel
+    {
+        $this->postalCode = $postalCode;
+        return $this;
+    }
+
+    public function wantsNewsletter(): bool
+    {
+        return $this->wantsNewsletter;
+    }
+
+    public function getLanguage(): Language
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(string $language): AbstractCustomerModel
+    {
+        try {
+            $this->language = Language::from($language);
+            return $this;
+        } catch (\ValueError $exception) {
+            throw new \Exception("Code de langue invalide",400);
+        }
+    }
+
     public function getCompanyName(): string
     {
         return $this->companyName;
@@ -32,33 +144,21 @@ class CompanyCustomerModel extends AbstractCustomerModel
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getFirstname(): string
     {
         return $this->firstname;
     }
 
-    /**
-     * @param string $firstname
-     */
     public function setFirstname(string $firstname): void
     {
         $this->firstname = $firstname;
     }
 
-    /**
-     * @return string
-     */
     public function getLastname(): string
     {
         return $this->lastname;
     }
 
-    /**
-     * @param string $lastname
-     */
     public function setLastname(string $lastname): void
     {
         $this->lastname = $lastname;
--- src/Model/IndividualCustomerModel.php
+++ src/Model/IndividualCustomerModel.php
@@ -2,7 +2,9 @@
 
 namespace D4rk0snet\Coralguardian\Model;
 
-class IndividualCustomerModel extends AbstractCustomerModel
+use D4rk0snet\Coralguardian\Enums\Language;
+
+class IndividualCustomerModel
 {
     /**
      * @required
@@ -13,6 +15,116 @@ class IndividualCustomerModel extends AbstractCustomerModel
      * @required
      */
     private string $lastname;
+
+    /**
+     * @required
+     */
+    private string $address;
+
+    /**
+     * @required
+     */
+    private string $postalCode;
+
+    /**
+     * @required
+     */
+    private string $city;
+
+    /**
+     * @required
+     */
+    private string $country;
+
+    /**
+     * @required
+     */
+    private string $email;
+
+    /**
+     * @required
+     */
+    private bool $wantsNewsletter;
+
+    /**
+     * @required
+     */
+    private Language $language;
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): AbstractCustomerModel
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): AbstractCustomerModel
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): AbstractCustomerModel
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): AbstractCustomerModel
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPostalCode(): string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(string $postalCode): AbstractCustomerModel
+    {
+        $this->postalCode = $postalCode;
+        return $this;
+    }
+
+    public function wantsNewsletter(): bool
+    {
+        return $this->wantsNewsletter;
+    }
+
+    public function getLanguage(): Language
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(string $language): AbstractCustomerModel
+    {
+        try {
+            $this->language = Language::from($language);
+            return $this;
+        } catch (\ValueError $exception) {
+            throw new \Exception("Code de langue invalide",400);
+        }
+    }
 
     public function getFirstname(): string
     {
