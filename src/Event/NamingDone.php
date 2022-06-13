4ce478585b11377712d1baaa82ac14234729ec7a
Benoit Delboé <benoit@lescanumeriques.fr>
2022-06-13 13:22:08 +0200

    use naming done event for friend certificate

--- src/Event/NamingDone.php
+++ src/Event/NamingDone.php
@@ -8,29 +8,46 @@ use D4rk0snet\Certificate\Service\CertificateService;
 use D4rk0snet\Coralguardian\Enums\Language;
 use D4rk0snet\Coralguardian\Enums\SIBEvent;
 use D4rk0snet\FiscalReceipt\Service\FiscalReceiptService;
+use D4rk0snet\GiftCode\Entity\GiftCodeEntity;
 
 class NamingDone extends AbstractEmailEvent
 {
     private static function send(string         $email,
-                                Language       $lang,
-                                AdoptedProduct $adoptedProduct,
-                                int            $quantity,
-                                string         $fiscalReceiptUrl,
-                                string         $certificateUrl)
+                                 Language       $lang,
+                                 AdoptedProduct $adoptedProduct,
+                                 int            $quantity,
+                                 string         $fiscalReceiptUrl,
+                                 string         $certificateUrl,
+                                 bool           $fromGift)
     {
-        self::sendQuery($email, compact('lang', 'adoptedProduct', 'quantity', 'fiscalReceiptUrl', 'certificateUrl'));
+        self::sendQuery($email, compact('lang', 'adoptedProduct', 'quantity', 'fiscalReceiptUrl', 'certificateUrl', 'fromGift'));
     }
 
-    public static function sendEvent(AdoptionEntity $adoptionOrder)
+    public static function sendEvent(mixed $entity)
     {
-        self::send(
-            email: $adoptionOrder->getCustomer()->getEmail(),
-            lang: $adoptionOrder->getLang(),
-            adoptedProduct: $adoptionOrder->getAdoptedProduct(),
-            quantity: $adoptionOrder->getQuantity(),
-            fiscalReceiptUrl: FiscalReceiptService::getUrl($adoptionOrder->getUuid()),
-            certificateUrl: CertificateService::getUrl($adoptionOrder->getUuid())
-        );
+        if ($entity instanceof GiftCodeEntity) {
+            $adoption = $entity->getGiftAdoption();
+            self::send(
+                email: $entity->getFriend()->getFriendEmail(),
+                lang: $adoption->getLang(),
+                adoptedProduct: $adoption->getAdoptedProduct(),
+                quantity: $entity->getProductQuantity(),
+                fiscalReceiptUrl: "",
+                certificateUrl: CertificateService::getUrl($entity->getGiftCode(), true),
+                fromGift: true
+            );
+        } else {
+            self::send(
+                email: $entity->getCustomer()->getEmail(),
+                lang: $entity->getLang(),
+                adoptedProduct: $entity->getAdoptedProduct(),
+                quantity: $entity->getQuantity(),
+                fiscalReceiptUrl: FiscalReceiptService::getUrl($entity->getUuid()),
+                certificateUrl: CertificateService::getUrl($entity->getUuid()),
+                fromGift: false
+            );
+        }
+
     }
 
     protected static function getEventName(): SIBEvent
