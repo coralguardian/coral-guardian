1baa71c62be6838e368967bc4d5d974a6e5e0395
d4rk0s <gregory@d4rk0s.net>
2022-09-21 16:24:02 +0200

    bugfix

--- coral-guardian-main.php
+++ coral-guardian-main.php
@@ -15,11 +15,12 @@ use D4rk0snet\Coralguardian\Listener\NewAdoptionListener;
 use D4rk0snet\Coralguardian\Listener\NewGiftAdoptionListener;
 use D4rk0snet\Coralguardian\Plugin;
 use D4rk0snet\Coralguardian\Service\AdminService;
+use D4rk0snet\GiftCode\Enum\CoralGiftActions;
 
 add_action('plugins_loaded', [Plugin::class, "launchActions"]);
 add_action('admin_menu', [AdminService::class, "addTopMenu"]);
 add_action("admin_notices", [AdminService::class, "handleAdminNotices"]);
 add_action("init", [AdminService::class, "init"]);
 add_action('cli_init', [Plugin::class,'addCliCommand']);
-add_action(CoralAdoptionActions::NEW_GIFT_ADOPTION->value, [NewGiftAdoptionListener::class, 'doAction'], 10, 2);
+add_action(CoralGiftActions::GIFTADOPTION_GIFTCODE_CREATED->value, [NewGiftAdoptionListener::class, 'doAction'], 10, 2);
 add_action(CoralAdoptionActions::NEW_ADOPTION->value, [NewAdoptionListener::class, 'doAction'], 10, 2);
