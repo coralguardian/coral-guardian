c260b0a9e858870b5918e839d068e8181d9a06de
d4rk0s <gregory@d4rk0s.net>
2022-09-22 18:05:30 +0200

    bugfix

--- src/Listener/NewGiftAdoptionListener.php
+++ src/Listener/NewGiftAdoptionListener.php
@@ -3,14 +3,13 @@
 namespace D4rk0snet\Coralguardian\Listener;
 
 use D4rk0snet\Adoption\Entity\GiftAdoption;
-use D4rk0snet\Adoption\Models\GiftAdoptionModel;
 use D4rk0snet\CoralCustomer\Entity\CompanyCustomerEntity;
 use D4rk0snet\Coralguardian\Event\GiftCodeSent;
 use D4rk0snet\Coralguardian\Event\GiftOrder;
 
 class NewGiftAdoptionListener
 {
-    public static function doAction(GiftAdoptionModel $adoptionModel, GiftAdoption $giftAdoptionEntity)
+    public static function doAction(GiftAdoption $giftAdoptionEntity)
     {
         // Envoie du mail
         GiftOrder::sendEvent($giftAdoptionEntity);
