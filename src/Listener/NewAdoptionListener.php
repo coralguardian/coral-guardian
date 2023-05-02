20027ac6cdf2bc24dd0ef28f5c033ad1c13656a4
Gregory Collin <gregory@d4rk0s.net>
2023-05-02 18:28:09 +0200

    Bugfix

--- src/Listener/NewAdoptionListener.php
+++ src/Listener/NewAdoptionListener.php
@@ -5,6 +5,7 @@ namespace D4rk0snet\Coralguardian\Listener;
 use D4rk0snet\Adoption\Entity\AdoptionEntity;
 use D4rk0snet\Adoption\Models\AdoptionModel;
 use D4rk0snet\Coralguardian\Event\AdoptionOrder;
+use D4rk0snet\Coralguardian\Event\NamingDone;
 
 class NewAdoptionListener
 {
@@ -12,5 +13,11 @@ class NewAdoptionListener
     {
         // Envoie du mail
         AdoptionOrder::sendEvent($adoptionEntity);
+
+        // Est ce que le nommage a déjà eu lieu ?
+        // Si oui on envoi direct l'évènement naming_done
+        if(count($adoptionEntity->getAdoptees())) {
+            NamingDone::sendEvent($adoptionEntity);
+        }
     }
 }
